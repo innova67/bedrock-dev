8cf96fbb0e5b72eb9c06a9537d73aeba0323e0e2
innova67 <37458981+innova67@users.noreply.github.com>
2019-07-15 04:08:09 -0400

    Add files via upload

--- contentLoader.js
+++ contentLoader.js
@@ -77,5 +77,5 @@ xmlhttp.onreadystatechange = function() {
         }
     }
 };
-xmlhttp.open("GET", "../content/tutorials/test.json", true);
+xmlhttp.open("GET", "test.json", true);
 xmlhttp.send();
