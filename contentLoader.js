ef5c5a5d79670fd29ce458e940083c62703970ac
innova67 <37458981+innova67@users.noreply.github.com>
2019-07-15 02:23:26 -0400

    Add files via upload

---
+++ contentLoader.js
@@ -0,0 +1,73 @@
+var container = document.getElementById("fillButtons")
+
+/* new page test */
+var btnNew = document.getElementById("newpage")
+btnNew.addEventListener("click",createPage);
+
+function createPage(){
+    var opened = window.open("");
+    opened.document.write("<html><head><title>MyTitle</title></head><body>test</body></html>");
+}
+
+console.log("im here bitch")
+
+/* request information from the content folder and organize it */
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        
+        /* converting the json into an object */
+        var myObj = JSON.parse(this.responseText);
+
+        /* adding title */
+        var doc = document.getElementById("toFill")
+        doc.innerHTML = myObj.title;
+        
+        /* looking for different types of content */
+        for(i=0; i<myObj.length; i++){
+
+            /**
+             * basic function:
+             * first creates the desired element
+             * then creates a text holder to collect the text
+             * puts the text into the element
+             * puts the element into the final page
+             */
+
+            /* paragraphs */
+            if( myObj[i] == "p"){
+                var newpara = document.createElement("p");
+                var newtext = document.createTextNode(myObj[i].p);
+                newpara.appendChild(newtext);
+                doc.appendChild(newpara);
+            }
+            /* images */
+            else if( myObj[i] == "img"){
+                var newImg = document.createElement("img");
+                newImg.src = myObj[i].img.url;
+                newImg.alt = myObj[i].img.alt;
+                doc.appendChild(newImg);
+            }
+            /* warning */
+            else if( myObj[i] == "w"){
+                var newWarn = document.createElement("p");
+                newWarn.className = "warning";
+                var newtext = document.createTextNode(myObj[i].w);
+                newWarn.appendChild(newtext);
+                doc.appendChild(newWarn);
+            }
+            else if( myObj[i] == "code"){
+                var newCode = document.createElement("p");
+                newCode.className = "code";
+                var newtext = document.createTextNode(myObj[i].code);
+                newCode.appendChild(newtext);
+                doc.appendChild(newCode);
+            }
+            else{
+                console.log("something very strange is happening here :O")
+            }
+        }
+    }
+};
+xmlhttp.open("GET", "content/tutorials/test.json", true);
+xmlhttp.send();
