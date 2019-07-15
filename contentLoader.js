8b9bb0404a694445c05ac36137e866e2c49d7fef
innova67 <37458981+innova67@users.noreply.github.com>
2019-07-15 02:28:53 -0400

    Update contentLoader.js

--- contentLoader.js
+++ contentLoader.js
@@ -69,5 +69,5 @@ xmlhttp.onreadystatechange = function() {
         }
     }
 };
-xmlhttp.open("GET", "content/tutorials/test.json", true);
+xmlhttp.open("GET", "content/tutorials/test.txt", true);
 xmlhttp.send();
